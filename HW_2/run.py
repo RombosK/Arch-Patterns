f65468e732dcf106e694c3b1702a2a71b420a995
Roman <rombos1978@gmail.com>
2023-01-11 21:54:15 +0300

    11.01.23

--- HW_2/run.py
+++ HW_2/run.py
@@ -5,8 +5,8 @@ from urls import routes, fronts
 
 application = Framework(routes, fronts)
 
-with make_server('', 8080, application) as httpd:
-    print("Запуск сервера - порт 8080...")
+with make_server('', 8000, application) as httpd:
+    print("Запуск сервера - порт 8000...")
     httpd.serve_forever()
 
 
